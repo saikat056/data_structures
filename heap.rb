30c24aa113b5d5099025a8cd7d035907e4e7fef0
Saikat Sarkar <saiksark@cisco.com>
2019-10-21 20:11:35 -0600

    Modify heap to support nodes instead of values

--- heap.rb
+++ heap.rb
@@ -21,8 +21,8 @@ class Heap
 
   def heapify(index)
     extremest = index
-    extremest = @array[left(index)].send(@operator, @array[index]) ? left(index) : index if left(index) < @heap_size
-    extremest = @array[right(index)].send(@operator, @array[extremest]) ? right(index) : extremest if right(index) < @heap_size
+    extremest = @array[left(index)].key.send(@operator, @array[index].key) ? left(index) : index if left(index) < @heap_size
+    extremest = @array[right(index)].key.send(@operator, @array[extremest].key) ? right(index) : extremest if right(index) < @heap_size
     if(extremest != index)
       @array[extremest], @array[index] = @array[index], @array[extremest]
       heapify(extremest)
@@ -59,12 +59,12 @@ class Heap
     top_value
   end
 
-  def change_value(index, value)
+  def change_value(index, node)
     if index < 0 || index >= heap_size
       puts "Invalid Index"
       return
     end
-    @array[index] = value
+    @array[index] = node
     parent_index = parent(index)
     while parent_index >= 0 do
       heapify(parent_index)
@@ -72,10 +72,10 @@ class Heap
     end
   end
 
-  def insert(value)
-    @array.push(value)
+  def insert(node)
+    @array.push(node)
     @heap_size = @array.size
-    change_value(@heap_size-1, value)
+    change_value(@heap_size-1, node)
   end
 end
 
@@ -88,15 +88,15 @@ class MaxHeap < Heap
 
   def extract_maximum; extract_top; end
 
-  def increase_key(index, value) 
-    if @array[index] > value
+  def increase_key(index, node) 
+    if @array[index].key > node.key
       puts "Can't increase the value at index. As value is less than array value"
     else
-      change_value(index, value)
+      change_value(index, node)
     end
   end
 
-  def insert(value); super(value); end
+  def insert(node); super(node); end
 end
 
 class MinHeap < Heap
@@ -108,15 +108,15 @@ class MinHeap < Heap
 
   def extract_minimum; extract_top; end
 
-  def decrease_key(index, value) 
-    if @array[index] < value
+  def decrease_key(index, node) 
+    if @array[index].key < node.key
       puts "Can't decrease the value at index. As value is larger than array value"
     else
-      change_value(index, value)
+      change_value(index, node)
     end
   end
 
-  def insert(value); super(value); end
+  def insert(node); super(node); end
 end
 
 class RunningMedian
@@ -125,33 +125,41 @@ class RunningMedian
     @min_heap = MinHeap.new([])
   end
 
-  def insert(value)
+  def insert(node)
     first_heap, second_heap, top = @min_heap.heap_size > @max_heap.heap_size ? [@min_heap, @max_heap, :minimum] 
                                  : [@max_heap, @min_heap, :maximum]
     top_value = first_heap.send("#{top}")
-    if top_value && top_value.send(first_heap.operator, value)
+    if top_value && top_value.key.send(first_heap.operator, node.key)
       second_heap.insert(first_heap.send("extract_#{top}"))
-      first_heap.insert(value)
+      first_heap.insert(node)
     else
-      second_heap.insert(value)
+      second_heap.insert(node)
     end
   end
 
   def median
     return nil if @min_heap.heap_size == 0 && @max_heap.heap_size == 0
-    return @min_heap.minimum if @max_heap.heap_size == 0
-    return @max_heap.maximum if @min_heap.heap_size == 0
-    return ((@max_heap.maximum + @min_heap.minimum).to_f/2) if @max_heap.heap_size == @min_heap.heap_size
-    return @max_heap.heap_size > @min_heap.heap_size ? @max_heap.maximum : @min_heap.minimum
+    return @min_heap.minimum.key if @max_heap.heap_size == 0
+    return @max_heap.maximum.key if @min_heap.heap_size == 0
+    return ((@max_heap.maximum.key + @min_heap.minimum.key).to_f/2) if @max_heap.heap_size == @min_heap.heap_size
+    return @max_heap.heap_size > @min_heap.heap_size ? @max_heap.maximum.key : @min_heap.minimum.key
+  end
+end
+
+class Node
+  attr_accessor :key, :value
+  def initialize(key, value=nil)
+    @key = key
+    @value = value
   end
 end
 
 
-max_heap = MaxHeap.new([3,7,1,9,7,12,7,19,2])
-puts max_heap.sort
+max_heap = MaxHeap.new([Node.new(3),Node.new(7),Node.new(1),Node.new(9),Node.new(7),Node.new(12),Node.new(7),Node.new(19),Node.new(2)])
+puts max_heap.sort.map{|x| x.key}
 puts "++++++++++++++++"
-min_heap = MinHeap.new([3,7,1,9,7,12,7,19,2])
-puts min_heap.sort
+min_heap = MinHeap.new([Node.new(3),Node.new(7),Node.new(1),Node.new(9),Node.new(7),Node.new(12),Node.new(7),Node.new(19),Node.new(2)])
+puts min_heap.sort.map{|x| x.key}
 puts "++++++++++++++++"
 puts "Heap size: #{max_heap.heap_size}"
 
@@ -160,13 +168,13 @@ puts "Running median"
 puts "+++++++++++++++++++++++"
 
 running_median = RunningMedian.new
-running_median.insert(1)
-running_median.insert(2)
-running_median.insert(3)
-running_median.insert(4)
-running_median.insert(5)
-running_median.insert(6)
-running_median.insert(7)
+running_median.insert(Node.new(1))
+running_median.insert(Node.new(2))
+running_median.insert(Node.new(3))
+running_median.insert(Node.new(4))
+running_median.insert(Node.new(5))
+running_median.insert(Node.new(6))
+running_median.insert(Node.new(7))
 puts running_median.median
-running_median.insert(8)
+running_median.insert(Node.new(8))
 puts running_median.median
