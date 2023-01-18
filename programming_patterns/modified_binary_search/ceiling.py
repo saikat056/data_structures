d8ab4ba73e23392f3dff4efb0c484e9a914b759f
Saikat Sarkar <saikatsarkar@Saikats-MacBook-Pro.local>
2023-01-17 19:18:07 -0700

    Add new file

---
+++ programming_patterns/modified_binary_search/ceiling.py
@@ -0,0 +1,26 @@
+# Given an array of numbers sorted in an ascending order, find the ceiling of a given number key. The ceiling of the key will be the smallest element in the given array greater than or equal to the key.
+
+def ceiling(nums, key):
+    if key > nums[-1]: return -1
+
+    n = len(nums)
+    start, end = 0, n-1
+
+    while(start <= end):
+        mid = (start + end)//2
+
+        if key < nums[mid]:
+            end = mid - 1
+        elif key > nums[mid]:
+            start = mid + 1
+        else:
+            return mid
+
+    return start
+
+# Time complexity: O(log(N))
+# Space complexity: O(1)
+
+print(ceiling([4, 6, 10], 6))
+print(ceiling([1, 3, 8, 10, 15], 12))
+print(ceiling([4, 6, 10], 17))
